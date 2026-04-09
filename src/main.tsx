813e77b81c51aa0964c2f1724c04587d3d9c10f0
aiyah-meloken <aiyah_meloken@protonmail.com>
2026-04-09 16:21:41 +0800

    fix: add rehype-highlight to package deps

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,16 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import CardFormatPreview from "./CardFormatPreview";
 import { AuthProvider } from "./lib/authStore";
 
+const isPreview = window.location.search.includes("preview");
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <AuthProvider>
-    <App />
-  </AuthProvider>,
+  isPreview ? (
+    <CardFormatPreview />
+  ) : (
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  ),
 );
